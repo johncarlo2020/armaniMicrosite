89f067a4612f3cbb5f0ca410d14fcbcc37a2dbaa
whei2419 <joelnacionales24@gmail.com>
2025-02-11 23:36:05 +0800

    update microsite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+const startTrigger = document.getElementById('startTrigger');
+const firstStep = document.querySelector('.first');
+const secondStep = document.querySelector('.second');
+
+startTrigger.addEventListener('click', () => {
+    firstStep.classList.add('d-none');
+    secondStep.classList.remove('d-none');
+});
